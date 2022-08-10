c1431cb4a24e75f890e6b53994b0f4d66b9b4fa9
Yasir Gaji <gajiyasir@gmail.com>
2022-08-10 19:08:54 +0100

    bismillah

---
+++ .history/state-cities_20220810183841.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Plugin Name: States and cities
+ * Plugin URI: https://github.com/YasirGaji/state-cities.git
+ * Author: Yasir Gaji
+ * Author URI: https://github.com/YasirGaji/state-cities.git
+ * Description: Adds a thrid level city to state
+ * Version: 0.1.0
+ * License: GPL2 or later
+ * License URL: http://www.gnu.org/licenses/gpl-2.0.txt
+ * text-domain: states-cities
+*/
+
+defined( 'ABSPATH') or die('Please get proper access');
+
+/**
+ * Add cities to Woocommerce. Needs WC City Select (https://wordpress.org/plugins/wc-city-select/) plugin installed.
+ * Replace XX with the country code. Instead of YYY, ZZZ use actual state codes.
+ * Pick countried from https://github.com/woocommerce/woocommerce/blob/master/i18n/countries.php
+ * Pick states from https://github.com/woocommerce/woocommerce/blob/master/i18n/states.php
+ */ 
+
+// Add aplugin notice to check for WC city Select as active plugin.
+add_action( 'plugins_loaded', 'check_for_wc_city_select_active' );
+
+function check_for_wc_city_select_active() {
+    if( ! class_exists( 'WC_City_Select' ) ) {
+        add_action( 'admin_notices', 'notice_wc_city_select_activate' );
+        return;
+    }
+
+    add_filter( 'wc_city_select_cities', 'techiepress_my_cities' );
+}
+
+function notice_wc_city_select_activate() {
+    $message = sprintf( __( 'States and cities for Woocommerce requires the <a href="%s">WC City Select</a> plugin to be active', 'states-and-cities-for-woocommerce' ), 'https://wordpress.org/plugins/wc-city-select/' );
+    printf( '<div class="error notice-error notice is-dismissible"><p>%s</p></div>', $message );
+}
+
+function techiepress_my_cities( $cities ) {
+
+    $cities['NG'] = array(
+        'LA' => array(
+            'Sakodi',
+            'Bluetag'
+        ),
+        'UG102' => array(
+            'Nakawa',
+            'Makindye'
+        )
+    );
+    return $cities;
+}
